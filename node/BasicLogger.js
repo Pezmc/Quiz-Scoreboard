e80e722e42edfc9cba77b77af9d687c10184ea8a
Pez Cuckow <email@pezcuckow.com>
2014-03-17 22:15:42 +0000

    Add logging level and a warning level

--- node/BasicLogger.js
+++ node/BasicLogger.js
@@ -1,16 +1,31 @@
 var colors = require('colors');
 
+// 0 - error, 1 - warn, 2 - info, 3 - debug
+var logLevel = 4;
+
 // Very basic info/debug/error messages
 exports.error = function(message, data) {
-  printMessage("[ERROR]\t".red + message, data);
+  if(logLevel >= 0)
+    printMessage("[ERROR]\t".red + message, data);
+}
+
+exports.warn = function(message, data) {
+  if(logLevel >= 1)
+    printMessage("[WARN]\t".yellow + message, data);
 }
 
 exports.info = function(message, data) {
+  if(logLevel >= 2)
   printMessage("[INFO]\t".cyan + message, data);
 }
 
 exports.debug = function(message, data) {
-  printMessage("[DEBUG]\t".green + message, data);
+  if(logLevel >= 3)
+    printMessage("[DEBUG]\t".green + message, data);
+}
+
+exports.setLogLevel = function(level) {
+  logLevel = level;
 }
 
 var printMessage = function(message, data) {
