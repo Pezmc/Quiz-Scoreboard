c98daa0e989f5f9470df04a97abbd51954d44900
Pez Cuckow <email@pezcuckow.com>
2017-03-23 12:32:26 +0000

    Whitespace cleanup

--- js/tables.js
+++ js/tables.js
@@ -11,15 +11,15 @@
 /////// Local Storage //////
 
 (function($){
-  
+
     var getStoreName = function(name) {
       return "table" + name;
     },
-    
+
     isSaved = function(name) {
       return localStorage.getItem(getStoreName(name)) != null
     },
-    
+
     isNumber = function(n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     },
@@ -38,10 +38,10 @@
 
     // Extend the jquery object
     $.extend($.fn, {
-      
+
       // Save object to local storage
       save: function(name){
-        var table = $(this);          
+        var table = $(this);
         if(table.prop("tagName") != 'TABLE') return console.log("You can only save tables");
 
         if (typeof(localStorage) == 'undefined' ) {
@@ -57,26 +57,26 @@
           }
         }
       },
-      
+
       load: function(name) {
         if(isSaved(name))
           $(this).html(localStorage.getItem(getStoreName(name)));
         else
           alert("Error: No table named "+name+", has been saved!");
       },
-      
+
       delete: function(name) {
         localStorage.removeItem(getStoreName(name));
       },
-            
+
       isSaved: function(name) {
         return isSaved(name);
       },
-        
+
       // Take a table and order it by the column with anim:sort
       animatedSort: function() {
         var table = $(this);
-        
+
         //What column are we ordering on?
         var sortIndex = table.find(".anim\\:sort").index();
 
@@ -99,17 +99,17 @@
             return $(this).index() === idIndex;
         });
       },
-        
+
       // Update the ranks (number) of the table
       updateRank: function() {
         var table = $(this);
-        
+
         var index = table.find(".anim\\:position").index();
         if(index < 0) index = 1;
         else index++;
 
         var scoreIndex = table.find(".anim\\:sort").index();
-        if(scoreIndex < 0) scoreIndex = 3; 
+        if(scoreIndex < 0) scoreIndex = 3;
         else scoreIndex++;
 
         var position = 0;
@@ -118,20 +118,20 @@
         var skippedRows = 0;
 
         $("tbody tr", table).each(function() {
-            var cell = $("td:nth-child(" + index + ")", this);
-            var totalScore = parseFloat($("td:nth-child(" + scoreIndex + ")", this).text());
+          var cell = $("td:nth-child(" + index + ")", this);
+          var totalScore = parseFloat($("td:nth-child(" + scoreIndex + ")", this).text());
 
-            // ensure teams with matching scores get the same position
-            if(totalScore != previousTotalScore) {
-              position += 1 + skippedRows;
-              previousTotalScore = totalScore;
-              skippedRows = 0;
-            } else {
-              skippedRows++;
-            }
+          // ensure teams with matching scores get the same position
+          if(totalScore != previousTotalScore) {
+            position += 1 + skippedRows;
+            previousTotalScore = totalScore;
+            skippedRows = 0;
+          } else {
+            skippedRows++;
+          }
 
-            // only change if needed
-            if (parseInt(cell.text()) != position) cell.text(position);
+          // only change if needed
+          if (parseInt(cell.text()) != position) cell.text(position);
         });
       }
     });
